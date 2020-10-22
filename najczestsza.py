70925850ff1e5e71f2929882a4154eb7424e8582
Maciej Dems <maciej.dems@p.lodz.pl>
2020-10-22 10:24:41 +0200

    Szablon ćwiczenia

---
+++ najczestsza.py
@@ -0,0 +1,25 @@
+﻿"""
+Proszę napisać program, który wczyta linijkę tekstu, a następnie wypisze
+najczęstszą literę w nim występującą. Program powinien ignorować wielkość
+liter, a najczęstsza litera powinna być wydrukowana jako wielka. Jeżeli
+najczęstsza litera występuje kilkukrotnie, wystarczy wypisać dowolną z nich.
+(dla chętnych — w takim wypadku w kolejnych liniach wypisać wszystkie litery,
+o których można powiedzieć, że są najczęstsze)
+
+Przykład:
+
+Podaj tekst: W Szczebrzeszynie chrząszcz brzmi w trzcinie
+Z
+
+W wersji zaawansowanej:
+
+Podaj tekst: Abba
+A
+B
+
+Wskazówka: proponuję stworzyć słownik, przyporządkowujący danej literze ilość
+jej wystąpień. Ilość wystąpień danej litery można poznać za pomocą metody
+`count` dla łańcucha tekstowego (proszę samodzielnie wyszukać jej dokumentację).
+"""
+
+# tutaj należy umieścić rozwiązanie
---
+++ usun3.py
@@ -0,0 +1,14 @@
+﻿"""
+Nazwa pliku:  `usun3.py`
+
+Proszę napisać program, który wczyta co tekst z klawiatury, a następnie usunie
+z niego co trzeci znak (zaczynając od trzeciego). Pozostałe znaki wydrukuje
+na ekranie.
+
+Przykład:
+
+Podaj tekst: Ala ma kota.
+Al m kta
+"""
+
+# tutaj należy umieścić rozwiązanie
